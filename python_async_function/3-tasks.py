5e11542cc45289038a1a8e48392fdb1f17f963ee
kaidoracer-dev <killian.lb@protonmail.com>
2026-04-11 18:31:07 +0200

    fix

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Module that creates an asyncio Task from wait_random."""
+"""Module that creates an asyncio Task from wait_random"""
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """Return an asyncio Task for wait_random."""
+    """Return an asyncio Task for wait_random"""
     return asyncio.get_event_loop().create_task(wait_random(max_delay))
