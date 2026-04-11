0574b27f65ed5b2c151d2a5c548a3607c63c60d9
kaidoracer-dev <killian.lb@protonmail.com>
2026-04-11 17:52:22 +0200

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module that provides a function to add two floats"""
 
+
 def add(a: float, b: float) -> float:
     """Add two floats and return their sum"""
     return a + b
