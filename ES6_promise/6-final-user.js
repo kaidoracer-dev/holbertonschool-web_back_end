f9ba6c25d79ee82a0458c34bd9e69a211a2561ce
kaidoracer-dev <killian.lb@protonmail.com>
2026-05-08 17:52:24 +0200

    fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,23 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(filename),
-    ]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value,
+      };
+    }
+
+    return {
+      status: result.status,
+      value: result.reason.toString(),
+    };
+  });
 }
